456f9b204869f760bb3db2d72b131816c6afb218
CraftOldW <1053474528@qq.com>
2025-04-19 20:56:57 +0800

    准备检查正确性

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # main.cpp train.cpp guessing.cpp md5.cpp
-CPP_FILES=(
-    "main_simd.cpp"
-    "md5_simd.cpp"
-    "train.cpp"  
-    "guessing.cpp"
+# CPP_FILES=(
+#     main_simd.cpp
+#     md5_simd.cpp
+#     guessing.cpp
+#     md5.cpp
+# )
 
-)   
+CPP_FILES=(
+    main.cpp
+    train.cpp 
+    guessing.cpp 
+    md5.cpp
+)
 
 # 获取脚本的当前目录
 SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
@@ -18,16 +24,17 @@ BUILD_DIR="$SCRIPT_DIR"    # 输出的可执行文件放到脚本所在目录
 # 创建目标文件夹（一般已经存在，这里保险起见）
 mkdir -p "$BUILD_DIR"
 
-# 手动指定要编译的 .cpp 文件（你可以在这里手动添加需要的文件）
-# 例如：CPP_FILES=("file1.cpp" "file2.cpp" "file3.cpp")
-
-
 # 拼接完整路径到文件
 for i in "${!CPP_FILES[@]}"; do
     CPP_FILES[$i]="$SCRIPT_DIR/${CPP_FILES[$i]}"
 done
+
+# 获取优化等级（从命令行参数中取，比如 -O1、-O2、-O3）
+OPT_LEVEL="$1"  # 如果你没传参数，这就是空字符串
+
 # 编译指定的 .cpp 文件为一个名为 main 的可执行文件
-g++ "${CPP_FILES[@]}" -o "$BUILD_DIR/main"
+echo "Compiling with: g++ ${CPP_FILES[*]} -o $BUILD_DIR/main $OPT_LEVEL"
+g++ "${CPP_FILES[@]}" -o "$BUILD_DIR/main" $OPT_LEVEL
 
 # 检查编译结果
 if [ $? -eq 0 ]; then
--- results/使用SIMD_test.e
+++ results/使用SIMD_test.e
@@ -1,6 +1,6 @@
-[1] 19:04:46 [SUCCESS] master_ubss1
+[1] 20:36:06 [SUCCESS] master_ubss1
 
 Authorized users only. All activities may be monitored and reported.
 
 Authorized users only. All activities may be monitored and reported.
-[1] 19:04:47 [SUCCESS] master_ubss1
+[1] 20:36:07 [SUCCESS] master_ubss1
