9c9fc07f383604a44201709b95ad749cf908ff17
CraftOldW <craftoldwmathandacg@gmail.com>
2025-04-22 11:32:35 +0800

    尝试avx，似乎效果挺好

---
+++ main_avx.cpp
@@ -0,0 +1,95 @@
+#include "PCFG.h"
+#include <chrono>
+#include <fstream>
+#include "md5.h"
+#include "md5_avx.h"  // 需要创建这个新的头文件
+#include <iomanip>
+#include <immintrin.h> // AVX 指令集头文件
+
+using namespace std;
+using namespace chrono;
+
+int GENERATE_N = 10000000;
+int NUM_PER_HASH = 1000000;
+
+// 编译指令
+// g++ main_avx.cpp train.cpp guessing.cpp md5.cpp md5_avx.cpp -o main_avx -mavx2 -O3
+
+int main()
+{
+    double time_hash = 0;  
+    double time_guess = 0; 
+    double time_train = 0; 
+    PriorityQueue q;
+    auto start_train = system_clock::now();
+    q.m.train(".\\guessdata\\Rockyou-singleLined-full.txt");
+    q.m.order();
+    auto end_train = system_clock::now();
+    auto duration_train = duration_cast<microseconds>(end_train - start_train);
+    time_train = double(duration_train.count()) * microseconds::period::num / microseconds::period::den;
+
+    q.init();
+    cout << "here" << endl;
+    int curr_num = 0;
+    auto start = system_clock::now();
+    int history = 0;
+    
+    while (!q.priority.empty())
+    {
+        q.PopNext();
+        q.total_guesses = q.guesses.size();
+        if (q.total_guesses - curr_num >= 100000)
+        {
+            cout << "Guesses generated: " << history + q.total_guesses << endl;
+            curr_num = q.total_guesses;
+
+            // 在此处更改实验生成的猜测上限
+            int generate_n=GENERATE_N;
+            if (history + q.total_guesses > GENERATE_N)
+            {
+                auto end = system_clock::now();
+                auto duration = duration_cast<microseconds>(end - start);
+                time_guess = double(duration.count()) * microseconds::period::num / microseconds::period::den;
+                cout << "Guess time:" << time_guess - time_hash << " seconds" << endl;
+                cout << "Hash time:" << time_hash << " seconds" << endl;
+                cout << "Train time:" << time_train << " seconds" << endl;
+                break;
+            }
+        }
+        
+        if (curr_num > 1000000)
+        {
+            cout << "Start performing the hash calculation..." << endl;
+            auto start_hash = system_clock::now();
+            
+            
+            for(size_t i = 0; i < q.guesses.size(); i += 8) {
+                string passwords[8] = {"", "", "", "", "", "", "", ""};
+                
+                // cout << "batch processing " << (i/8) << " (indexes " << i << ")" << endl;
+                
+                // 填充密码数组（考虑边界情况）
+                for (int j = 0; j < 8 && (i + j) < q.guesses.size(); ++j) {
+                    passwords[j] = q.guesses[i + j];
+                }
+                if (i >= 100000){
+                    break;
+                }
+                // 使用AVX版本计算MD5
+                __m256i state[4]; // 8个密码的状态向量
+                MD5Hash_AVX(passwords, state);
+            }
+            
+
+            auto end_hash = system_clock::now();
+            auto duration = duration_cast<microseconds>(end_hash - start_hash);
+            time_hash += double(duration.count()) * microseconds::period::num / microseconds::period::den;
+
+            history += curr_num;
+            curr_num = 0;
+            q.guesses.clear();
+        }
+    }
+    
+    return 0;
+}
