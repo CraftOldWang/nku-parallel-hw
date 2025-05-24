14a24169a00c71f244b6c601bf3b307558cf75c3
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-24 10:15:19 +0800

    修改了文件，从而可以本地运行。

--- correctness_guess.cpp
+++ correctness_guess.cpp
@@ -19,7 +19,11 @@ int main()
     double time_train = 0; // 模型训练的总时长
     PriorityQueue q;
     auto start_train = system_clock::now();
+#ifdef _WIN32
+    q.m.train(".\\guessdata\\small_Rockyou-singleLined-full.txt");
+#else
     q.m.train("/guessdata/Rockyou-singleLined-full.txt");
+#endif
     q.m.order();
     auto end_train = system_clock::now();
     auto duration_train = duration_cast<microseconds>(end_train - start_train);
@@ -29,7 +33,11 @@ int main()
     
     // 加载一些测试数据
     unordered_set<std::string> test_set;
+#ifdef _WIN32
+    ifstream test_data(".\\guessdata\\small_Rockyou-singleLined-full.txt");
+#else
     ifstream test_data("/guessdata/Rockyou-singleLined-full.txt");
+#endif
     int test_count=0;
     string pw;
     while(test_data>>pw)
