544379a958c40c3ef2b8209f99d4c861214271bf
CraftOldW <1053474528@qq.com>
2025-05-28 17:29:38 +0800

    还是在做实验；调整了不用的文件

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define NOT_USING_STRING_ARR // 定义宏，或者注释掉以禁用
-#define USING_ALIGNED
+// #define NOT_USING_STRING_ARR // 定义宏，或者注释掉以禁用
+// #define USING_ALIGNED
 
-
-#define USING_SIMD // 定义宏以启用SIMD MD5计算
+// 也许可以把所有设置放这里， 然后我们就只需要几个文件而不是一堆。
+// #define USING_SIMD // 定义宏以启用SIMD MD5计算
 
 #define THREAD_NUM 8 // 线程池的线程数
 
