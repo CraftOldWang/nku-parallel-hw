4417c8cfb7f8f6e30b109507c7243190c759fbe5
CraftOldW <craftoldwmathandacg@gmail.com>
2025-06-04 11:40:47 +0800

    开始多进程实验

--- config.h
+++ config.h
@@ -4,9 +4,13 @@
 // #define NOT_USING_STRING_ARR // 定义宏，或者注释掉以禁用
 // #define USING_ALIGNED
 
-// 也许可以把所有设置放这里， 然后我们就只需要几个文件而不是一堆。
+
 // #define USING_SIMD // 定义宏以启用SIMD MD5计算
 
-#define THREAD_NUM 8 // 线程池的线程数
+#define USING_MPI // 定义宏以启用MPI并行处理
+
+
+// #define THREAD_NUM 8 // 线程池的线程数
+
 
 #endif
